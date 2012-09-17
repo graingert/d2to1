97d709da902ae307a46f68d0e067e8ae99c4fdca
Erik M. Bray <embray@stsci.edu>
2012-09-17 18:55:14 -0400

    Add changelog entry for 3620f13 and fix syntax error

--- d2to1/util.py
+++ d2to1/util.py
@@ -16,8 +16,8 @@ from collections import defaultdict
 import distutils.ccompiler
 
 from distutils import log
-from distutils.errors import DistutilsOptionError, DistutilsModuleError \
-        DistutilsFileError
+from distutils.errors import (DistutilsOptionError, DistutilsModuleError,
+                              DistutilsFileError)
 from setuptools.dist import Distribution
 from setuptools.extension import Extension
 try:
