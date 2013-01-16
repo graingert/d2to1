30f7722f0cf4d0f8ffaf42e8897a29b007423976
Erik M. Bray <embray@stsci.edu>
2013-01-16 12:15:44 -0500

    Fix typos in docstrings

--- d2to1/tests/test_core.py
+++ d2to1/tests/test_core.py
@@ -7,7 +7,7 @@ VERSION = '0.1.dev'
 class TestCore(D2to1TestCase):
     def test_setup_py_version(self):
         """
-        Test that the `./setupy.py --version` command returns the correct
+        Test that the `./setup.py --version` command returns the correct
         value without balking.
         """
 
@@ -17,7 +17,7 @@ class TestCore(D2to1TestCase):
 
     def test_setup_py_keywords(self):
         """
-        Test that the `./setupy.py --keywords` command returns the correct
+        Test that the `./setup.py --keywords` command returns the correct
         value without balking.
         """
 
